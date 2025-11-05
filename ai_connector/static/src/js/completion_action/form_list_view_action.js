562af65d071f45c1070f7434286b9f6f8960a101
Michel Perrocheau <michel@scalizer.fr>
2025-11-05 15:16:42 +0100

    [FIX] display completion actions only for AI group users

--- ai_connector/static/src/js/completion_action/form_list_view_action.js
+++ ai_connector/static/src/js/completion_action/form_list_view_action.js
@@ -5,6 +5,7 @@ import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 import { ActionMenus, ACTIONS_GROUP_NUMBER } from "@web/search/action_menus/action_menus";
 import { useService } from "@web/core/utils/hooks";
 import { Component } from "@odoo/owl";
+import { user } from "@web/core/user";
 
 
 export class RunCompletion extends Component {
@@ -36,6 +37,9 @@ export const CompletionActionMenus = {
     async getActionItems(props) {
         const items = await super.getActionItems(props);
         if (!('getActiveIds' in this.props) || !this.props.getActiveIds()) {
+            return items;
+        }
+        if (!await user.hasGroup("ai_connector.group_ai_user")) {
             return items;
         }
         const results = await this.orm.call("ai.completion", "get_model_completions", [this.env.searchModel.resModel]);
